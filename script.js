b6ddd9e0e6c59fd0bf592dfe99bf88879c07a127
RK11279 <riyakama09@gmail.com>
2024-11-28 23:37:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("accept").addEventListener("click", function () {
+    window.location.href = "stage1/index.html";  // Link to the second stage page
+});
+
+document.getElementById("reject").addEventListener("click", function () {
+    alert("So you rejected? Guess you don't have the guts for this challenge! 😏 Maybe next time...");
+window.location.href = "https://youtu.be/V4RRBFFkkJA?si=y-bAAhsW8Bx01UJY";
+});
